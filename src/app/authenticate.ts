457a0e1f38d40cbde2e60b20cce1bd93b6badfda
JP <jsniezek@crowdskout.com>
2015-01-03 12:35:41 -0500

    Removed boilerplate authentication

--- src/app/authenticate.ts
+++ src/app/authenticate.ts
@@ -6,12 +6,6 @@ import app = require('./app');
 
 app.run(function($rootScope, $state, NoAuthRoutes, $timeout) {
   $rootScope.$on('$stateChangeStart', function(event, toState) {
-    var token = localStorage.getItem('token');
-    if(NoAuthRoutes.indexOf(toState.name) === -1 && !token) {
-      event.preventDefault();
-      $timeout(function() {
-        $state.go("login");
-      });
-    }
+    // authenticate
   });
 });
