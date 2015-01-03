09eb73f412c29d52d4ac890986e222baf787c744
JP <jsniezek@crowdskout.com>
2015-01-03 12:33:12 -0500

    Removed compiled js and .map files

--- src/app/bootstrap.js
+++
@@ -1,7 +0,0 @@
-/// <reference path="../../defs/_app.d.ts" />
-define(["require", "exports", 'jquery', 'angular', "./authenticate", "./lazy", "./menus/Sidebar"], function (require, exports, $, angular) {
-    $(document).ready(function () {
-        angular.bootstrap(document, ['app']);
-    });
-});
-//# sourceMappingURL=bootstrap.js.map
--- src/app/menus/Sidebar.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="../../../defs/_app.d.ts" />
-define(["require", "exports", '../app', '../../lib/Menu'], function (require, exports, app, Menu) {
-    app.controller('SidebarController', function ($scope, Sidebar) {
-        $scope.menu = Sidebar;
-    });
-    var sidebar = new Menu('Sidebar');
-    return sidebar;
-});
-//# sourceMappingURL=Sidebar.js.map
